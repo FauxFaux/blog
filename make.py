7d5280a4fa3daa1477bb629f21a46b948421f61c
Chris West (Faux) <git@goeswhere.com>
2015-03-02 17:51:47 +0000

    apparently I rewrote it by hand

--- make.py
+++ make.py
@@ -2,6 +2,20 @@
 
 from markdown import markdown
 from glob import glob
+from collections import namedtuple
+from datetime import datetime
+from dateutil import parser
+import os
+
+Item = namedtuple('Item', ['title', 'slug', 'date', 'content'])
+
+def open_out(path):
+    path = 'out/' + path
+    try:
+        os.makedirs(os.path.dirname(path))
+    except FileExistsError:
+        pass
+    return open(path, 'w')
 
 files = {}
 for name in glob("md/*.md"):
@@ -16,8 +30,18 @@ for name in glob("md/*.md"):
         slug = headers['slug']
         if not slug:
             raise 'No slug: ' + name
-        headers['content'] = f.read()
-        files[slug] = headers
+        files[slug] = Item(headers['title'], slug, parser.parse(headers['date']), f.read())
 
-print(len(files))
+def url_of(item):
+    return item.date.strftime("%Y/%m/") + item.slug + "/"
 
+
+for slug, item in files.items():
+    sub = url_of(item)
+    with open_out(sub + "index.html") as f:
+        f.write(markdown(item.content))
+
+with open_out("index.html") as f:
+    for item in sorted(files.values(), reverse=True, key=lambda item: item.date):
+        f.write('<a href="' + url_of(item) + '"><h1>' + item.title + '</h1></a>\n')
+        f.write(markdown(item.content))
