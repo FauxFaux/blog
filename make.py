a7540dac4b32cba3f2a6736008f2d531296d2058
Chris West (Faux) <git@goeswhere.com>
2026-02-22 14:36:12 +0000

    chore: uv format

--- make.py
+++ make.py
@@ -10,37 +10,40 @@ from glob import glob
 # python3-jinja2
 import jinja2
 import pytz
+
 # python3-slimmer
-#import slimmer
+# import slimmer
 from dateutil import parser
+
 # pip3 install --user feedgen
 from feedgen.feed import FeedGenerator
+
 # python3-markdown
 from markdown import markdown
 
-templates = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+templates = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
 
-feed_root = 'https://blog.goeswhere.com'
+feed_root = "https://blog.goeswhere.com"
 fg = FeedGenerator()
 fg.id(feed_root)
-fg.title('Faux\' Blog')
-fg.author({'name': 'Chris West (Faux)'})
-fg.language('en')
-fg.link(href=feed_root, rel='alternate')
-fg.description('Faux\' (mostly) technical blog')
+fg.title("Faux' Blog")
+fg.author({"name": "Chris West (Faux)"})
+fg.language("en")
+fg.link(href=feed_root, rel="alternate")
+fg.description("Faux' (mostly) technical blog")
 
-tz = pytz.timezone('Europe/London')
+tz = pytz.timezone("Europe/London")
 
-Item = namedtuple('Item', ['title', 'slug', 'date', 'content', 'mtime'])
+Item = namedtuple("Item", ["title", "slug", "date", "content", "mtime"])
 
 
 def open_out(path):
-    path = 'out/' + path
+    path = "out/" + path
     try:
         os.makedirs(os.path.dirname(path))
     except FileExistsError:
         pass
-    return open(path, 'w')
+    return open(path, "w")
 
 
 files = {}
@@ -53,12 +56,16 @@ for name in glob("md/*.md"):
                 break
             bits = [x.strip() for x in s.split(":", 1)]
             headers[bits[0]] = bits[1]
-        slug = headers['slug']
+        slug = headers["slug"]
         if not slug:
-            raise 'No slug: ' + name
-        files[slug] = Item(headers['title'], slug,
-                           parser.parse(headers['date']), f.read(),
-                           os.path.getmtime(name))
+            raise "No slug: " + name
+        files[slug] = Item(
+            headers["title"],
+            slug,
+            parser.parse(headers["date"]),
+            f.read(),
+            os.path.getmtime(name),
+        )
 
 
 def url_of(item):
@@ -67,68 +74,79 @@ def url_of(item):
 
 def write_page(path, template, title, **args):
     if not title:
-        title = 'Faux\' blog'
+        title = "Faux' blog"
     else:
-        title = 'Faux\' blog: ' + title
+        title = "Faux' blog: " + title
 
     with open_out(path) as f:
         f.write(
-            #slimmer.html_slimmer(
-                templates.get_template('index.html').render(
-                    content=templates.get_template(template).render(args),
-                    title=title))
+            # slimmer.html_slimmer(
+            templates.get_template("index.html").render(
+                content=templates.get_template(template).render(args), title=title
+            )
+        )
 
 
 def render_post(item, full=False):
     content = item.content
     more = False
-    if '<!--more-->' in item.content:
+    if "<!--more-->" in item.content:
         more = not full
         if more:
-            content = re.sub('<!--more-->.*', '', content, flags=re.DOTALL)
+            content = re.sub("<!--more-->.*", "", content, flags=re.DOTALL)
         else:
-            content = re.sub('<!--more-->', '<a name="more"></a>', content)
+            content = re.sub("<!--more-->", '<a name="more"></a>', content)
 
-    return templates.get_template('post.html').render(
+    return templates.get_template("post.html").render(
         content=markdown(content),
         url=url_of(item),
-        date=item.date.strftime('%Y-%m-%d'),
+        date=item.date.strftime("%Y-%m-%d"),
         more=more,
-        title=item.title)
+        title=item.title,
+    )
 
 
 for slug, item in files.items():
     sub = url_of(item)
-    write_page(sub + 'index.html', 'single.html', item.title,
-               render_post=render_post,
-               item=item)
+    write_page(
+        sub + "index.html",
+        "single.html",
+        item.title,
+        render_post=render_post,
+        item=item,
+    )
 
 
 def write_list(base_path, items, title):
     items = sorted(items, reverse=True, key=lambda item: item.date)
     per_page = 6
-    chunks = [items[x:x + per_page] for x in range(0, len(items), per_page)]
+    chunks = [items[x : x + per_page] for x in range(0, len(items), per_page)]
     for idx, chunk in enumerate(chunks):
         path = base_path
         prev = None
         next = None
         if 0 != idx:
-            path = path + 'page/' + str(idx + 1) + '/'
+            path = path + "page/" + str(idx + 1) + "/"
             if 1 != idx:
-                prev = base_path + 'page/' + str(idx) + '/'
+                prev = base_path + "page/" + str(idx) + "/"
             else:
                 prev = base_path
 
         if len(chunks) - 1 != idx:
-            next = base_path + 'page/' + str(idx + 2) + '/'
+            next = base_path + "page/" + str(idx + 2) + "/"
 
-        write_page(path + 'index.html', 'list.html', title,
-                   items=chunk,
-                   next=next, prev=prev,
-                   render_post=render_post)
+        write_page(
+            path + "index.html",
+            "list.html",
+            title,
+            items=chunk,
+            next=next,
+            prev=prev,
+            render_post=render_post,
+        )
 
 
-write_list('all/', files.values(), None)
+write_list("all/", files.values(), None)
 
 
 def by_date(format, items):
@@ -138,16 +156,20 @@ def by_date(format, items):
     return ret
 
 
-for by_dates in [by_date('%Y/%m/', files.values()), by_date('%Y/', files.values())]:
+for by_dates in [by_date("%Y/%m/", files.values()), by_date("%Y/", files.values())]:
     for month, items in by_dates.items():
         write_list(month, items, month)
 
 
 def write_index(items):
-    by_year = by_date('%Y', sorted(items, reverse=True, key=lambda item: item.date))
+    by_year = by_date("%Y", sorted(items, reverse=True, key=lambda item: item.date))
 
-    write_page('index.html', 'front.html', None,
-               by_year=sorted(by_year.items(), reverse=True, key=lambda pair: pair[0]))
+    write_page(
+        "index.html",
+        "front.html",
+        None,
+        by_year=sorted(by_year.items(), reverse=True, key=lambda pair: pair[0]),
+    )
 
 
 write_index(files.values())
@@ -158,20 +180,20 @@ for item in in_feed:
     fe = fg.add_entry()
     full_url = feed_root + url_of(item)
     fe.id(full_url)
-    fe.link(href=full_url, rel='alternate')
+    fe.link(href=full_url, rel="alternate")
     fe.title(item.title)
     fe.content(markdown(item.content))
     fe.updated(tz.localize(datetime.fromtimestamp(item.mtime)))
     fe.published(tz.localize(datetime.fromtimestamp(item.mtime)))
 
-os.makedirs('out/feed/atom')
-os.makedirs('out/feed/rss')
-fg.atom_file('out/feed/atom/index.xml')
-fg.rss_file('out/feed/rss/index.xml')
-fg.rss_file('out/feed/index.xml')
+os.makedirs("out/feed/atom")
+os.makedirs("out/feed/rss")
+fg.atom_file("out/feed/atom/index.xml")
+fg.rss_file("out/feed/rss/index.xml")
+fg.rss_file("out/feed/index.xml")
 
-for static in ['main.css']:
-    with open_out('static/' + static) as f:
+for static in ["main.css"]:
+    with open_out("static/" + static) as f:
         f.write((templates.get_template(static).render()))
 
-shutil.copytree('images', 'out/images')
+shutil.copytree("images", "out/images")
--- xml2static.py
+++ xml2static.py
@@ -3,14 +3,17 @@ from lxml import etree
 from datetime import datetime
 from sys import argv
 
-RSS_DATE_FORMAT = '%a, %d %b %Y %H:%M:%S %z'
+RSS_DATE_FORMAT = "%a, %d %b %Y %H:%M:%S %z"
+
 
 # http://foo/bar/baz/ -> baz
 def last_element_of_url(url):
     return url.split("/")[-2]
 
+
 def parse_rss_date(string):
     return datetime.strptime(string, RSS_DATE_FORMAT)
+
 
 with open(argv[1]) as f:
     tree = etree.parse(f)
@@ -22,11 +25,9 @@ for item in etree.ETXPath("//item")(tree):
     content = item.find("{http://purl.org/rss/1.0/modules/content/}encoded").text
 
     datafile = date.strftime("%Y-%m-%d-") + slug + ".md"
-    with open(datafile, 'w') as f:
+    with open(datafile, "w") as f:
         f.write("title: %s\n" % title)
         f.write("slug: %s\n" % slug)
         f.write("date: %s\n" % date)
         f.write("\n")
         f.write(content)
-
-
